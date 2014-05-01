51ed26fce7325f8312789783ae320aa85aff13c8
北川俊太 <kitagawa@timedia.co.jp>
2014-05-01 23:23:25 +0900

    Define insert! that is headed-list version

--- chapter-3/3.3.3/Representing-Tables.scm
+++ chapter-3/3.3.3/Representing-Tables.scm
@@ -4,7 +4,17 @@
       (cdr record)
       #f)))
 
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+      (set-cdr! record value)
+      (set-cdr! table
+                (cons (cons key value) (cdr table)))))
+  'ok)
+
 (define sample-table
   '(*table* (a . 1) (b . 2) (c . 3)))
 
-(print (lookup 'd sample-table))
+(define (main args)
+  (print (insert! 'd 4 sample-table))
+  (print (lookup 'd sample-table)))
