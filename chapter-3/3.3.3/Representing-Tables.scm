53875747f0727fb2c30ff736043d22a7a2c0820d
北川俊太 <kitagawa@timedia.co.jp>
2014-05-01 23:28:13 +0900

    Define make-table that is headed-list version

--- chapter-3/3.3.3/Representing-Tables.scm
+++ chapter-3/3.3.3/Representing-Tables.scm
@@ -12,8 +12,9 @@
                 (cons (cons key value) (cdr table)))))
   'ok)
 
-(define sample-table
-  '(*table* (a . 1) (b . 2) (c . 3)))
+(define (make-table)
+  (list '*table*))
+(define sample-table (make-table))
 
 (define (main args)
   (print (insert! 'd 4 sample-table))
