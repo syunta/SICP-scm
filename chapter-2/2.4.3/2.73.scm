7159fe7c9444c8da8f8e8bc1bda5bd4b31118255
syunta <kitagawa@timedia.co.jp>
2014-05-08 23:07:10 +0900

    Answer 2.73-b-product

--- chapter-2/2.4.3/2.73.scm
+++ chapter-2/2.4.3/2.73.scm
@@ -40,6 +40,29 @@
   (put 'deriv '+ deriv-sum)
   'done)
 
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(define (install-product-package)
+  (define (deriv-product operands var)
+    (let ((ex (apply make-product operands)))
+      (make-sum
+        (make-product (multiplier ex)
+                      (deriv (multiplicand ex) var))
+        (make-product (deriv (multiplier ex) var)
+                      (multiplicand ex)))))
+
+  (put 'deriv '* deriv-product)
+  'done)
+
 (define (main args)
   (print (install-sum-package))
-  (print (deriv '(+ x (+ x (+ x (+ 7 x)))) 'x)))
+  (print (install-product-package))
+  (print (deriv '(* (* x y) (+ x 3)) 'x)))
