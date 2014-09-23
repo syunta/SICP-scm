48708dac88dc976a52fd90c974245502fcaaf653
syunta <kitagawa@timedia.co.jp>
2014-09-23 18:08:13 +0900

    Add base program in order to answer 3.5

---
+++ chapter-3/3.1.2/3.5.scm
@@ -0,0 +1,18 @@
+(use srfi-27)
+
+(define (random x)
+  (/ (random-integer (* x 10000)) 10000))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+            (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
