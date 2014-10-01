3c5962ccdc0ae757fc8b7434e431c0ad5f10d61f
syunta <kitagawa@timedia.co.jp>
2014-10-01 22:56:42 +0900

    Answer 3.7

--- chapter-3/3.1.3/3.7.scm
+++ chapter-3/3.1.3/3.7.scm
@@ -15,3 +15,24 @@
                          m)))
       (lambda x "Incorrect password")))
   dispatch)
+
+(define (make-joint acc passwd new-passwd)
+  (if (not (equal? ((acc passwd 'withdraw) 0) "Incorrect password"))
+    (lambda (pw m)
+      (if (eq? pw new-passwd)
+        (acc passwd m)
+        (lambda x "Incorrect password")))
+    (error "Incorrect password -- MAKE-JOINT"
+           passwd)))
+
+(define peter-acc (make-account 100 'open-sesame))
+(define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+(define hoge-acc (make-joint paul-acc 'rosebud 'hoge))
+
+(define (main args)
+  (print ((paul-acc 'rosebud 'withdraw) 30))
+  (print ((peter-acc 'open-sesame 'withdraw) 10))
+  (print ((paul-acc 'rosebud 'deposit) 30))
+  (print ((peter-acc 'open-sesame 'deposit) 10))
+  (print ((hoge-acc 'hoge 'withdraw) 20))
+  (print ((peter-acc 'open-sesame 'withdraw) 10)))
