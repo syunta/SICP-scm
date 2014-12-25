bb07970982b965bc507b35e2955ff9e61fce057f
syunta <kitagawa@timedia.co.jp>
2014-12-25 10:52:05 +0900

    Modify table construction

--- chapter-3/3.3.3/3.26.scm
+++ chapter-3/3.3.3/3.26.scm
@@ -1,4 +1,4 @@
-; table construction design: (entry key left right next)
+; table construction design: (prev-node-value key left right next)
 ;
 ; (define *table* (make-table))
 ; (insert! '(4 5) 20 *table*)
@@ -12,33 +12,33 @@
 ;  |      |      |      |      |
 ;  V      V      |      |      V
 ; #f      4      |      |    (* *)->(* *)->(* *)->(* *)->(* /)
-;                |      |     |      |      |      |      |   
-;                |      |     V      V      V      V      V   
-;                |      |    20      5    (* /)  (* /)  (* /)     
-;                |      |                  |      |      |   
-;                |      |                  V      V      V   
-;                |      |                 #f     #f     #f 
-;                |      V                                  
-;                |    (* *)->(* *)->(* *)->(* *)->(* /)    
-;                |     |      |      |      |      |       
-;                |     V      V      V      V      V    
-;                |     5      5    (* /)  (* /)  (* /)      
-;                |                  |      |      |   
-;                |                  V      V      V   
-;                |                 #f     #f     #f   
-;                V                                    
-;              (* *)->(* *)->(* *)->(* *)->(* /)      
-;               |      |      |      |      |         
-;               V      V      V      V      V   
-;               2      2    (* /)  (* /)  (* *)->(* *)->(* *)->(* *)->(* /)  
-;                            |      |      |      |      |      |      |   
-;                            V      V      V      V      V      V      V   
-;                           #f     #f     18      8    (* /)  (* /)  (* /)     
-;                                                       |      |      |   
-;                                                       V      V      V   
-;                                                      #f     #f     #f
+;                |      |     |      |      |      |      |
+;                |      |     V      V      V      V      V
+;                |      |    #f      5    (* /)  (* /)  (* /)
+;                |      |                  |      |      |
+;                |      |                  V      V      V
+;                |      |                 #f     #f     20
+;                |      V
+;                |    (* *)->(* *)->(* *)->(* *)->(* /)
+;                |     |      |      |      |      |
+;                |     V      V      V      V      V
+;                |    #f      5    (* /)  (* /)  (* /)
+;                |                  |      |      |
+;                |                  V      V      V
+;                |                 #f     #f      5
+;                V
+;              (* *)->(* *)->(* *)->(* *)->(* /)
+;               |      |      |      |      |
+;               V      V      V      V      V
+;              #f      2    (* /)  (* /)  (* *)->(* *)->(* *)->(* *)->(* /)
+;                            |      |      |      |      |      |      |
+;                            V      V      V      V      V      V      V
+;                           #f     #f     #f      8    (* /)  (* /)  (* /)
+;                                                       |      |      |
+;                                                       V      V      V
+;                                                      #f     #f     16
 (define (make-table) (list #f))
-(define (make-tree) (list #f (make-table) (make-table) (make-table)))
+(define (make-node) (list #f (make-table) (make-table) (make-table)))
 
 (define (entry record) (car record))
 (define (key record) (cadr record))
@@ -52,10 +52,11 @@
 (define (empty? table) (null? (cdr table)))
 
 (define (lookup keys table)
-  (let ((record (lookup-record (car keys) table)))
-    (cond ((empty? record) #f)
-          ((null? (cdr keys)) (entry record))
-          (else (lookup (cdr keys) (next record))))))
+  (if (null? keys)
+    (entry table)
+    (let ((record (lookup-record (car keys) table)))
+      (cond ((empty? record) #f)
+            (else (lookup (cdr keys) (next record)))))))
 
 (define (lookup-record given-key table)
   (cond ((empty? table) table)
@@ -69,24 +70,19 @@
   (if (null? keys)
     (set-entry! table value)
     (let ((record (lookup-record (car keys) table)))
-      (if (empty? record)
-        (cond ((null? (cdr keys))
-               (set-cdr! record (make-tree))
-               (set-key! record (car keys))
-               (insert! (cdr keys) value record))
-              (else
-                (set-cdr! record (make-tree))
-                (set-key! record (car keys))
-                (insert! (cdr keys) value (next record))))
-        (cond ((null? (cdr keys))
-               (insert! (cdr keys) value record))
-              (else
-                (insert! (cdr keys) value (next record))))))))
+      (cond ((empty? record)
+             (set-cdr! record (make-node))
+             (set-key! record (car keys))
+             (insert! (cdr keys) value (next record)))
+            (else
+              (insert! (cdr keys) value (next record)))))))
 
 (define *table* (make-table))
 (insert! '(4 5) 20 *table*)
 (insert! '(2 8) 16 *table*)
 (insert! '(5) 5 *table*)
+(print *table*)
+;=> (#f 4 (#f 2 (#f) (#f) (#f 8 (#f) (#f) (16))) (#f 5 (#f) (#f) (5)) (#f 5 (#f) (#f) (20)))
 (print (lookup '(2 8) *table*))
 ;=> 16
 (print (lookup '(2 8 2) *table*))
