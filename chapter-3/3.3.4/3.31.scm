6953b667f8075114b6c84a63b0544b6ff287b770
syunta <kitagawa@timedia.co.jp>
2015-01-03 18:35:48 +0900

    Answer 3.31

---
+++ chapter-3/3.3.4/3.31.scm
@@ -0,0 +1,45 @@
+;  理由
+
+;half-adderの例の場合、carryをinverterに接続した際、eのsignal-valueを1に変更しなければならない。
+;procを呼び出さないとこの初期化がagendaに登録(2単位時間後)されず、誤った結果を印字してしまう。
+
+
+;  procを呼び出さないシミュレーション
+(add-load-path "." :relative) (load "simulator.scm")
+(add-load-path "../3.3.2" :relative) (load "queue.scm")
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+        (begin (set! signal-value new-value)
+               (call-each action-procedures))
+        'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures)))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+(set-signal! input-2 1)
+(propagate)
+
+;=> carry 11  New-value = 1
