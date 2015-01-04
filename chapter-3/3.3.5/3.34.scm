57ea65c6ffa4fb45a54b63b46fa88100858554d6
syunta <kitagawa@timedia.co.jp>
2015-01-04 20:49:51 +0900

    Answer 3.34

---
+++ chapter-3/3.3.5/3.34.scm
@@ -0,0 +1,19 @@
+(add-load-path "." :relative)
+(load "constraint_system.scm")
+
+(define (squarere a b)
+  (multiplier a a b))
+
+(define a (make-connector))
+(define b (make-connector))
+
+(squarere a b)
+
+(probe "a" a)
+(probe "b" b)
+
+(set-value! b 9 'user)
+
+; b = a^2 の式は、bが決まればaも決まらなければならない。
+; しかし、maltiplierは、2つ以上のコネクタに値が設定されていないと働かない。
+; bだけが決まっても残り2つのコネクタには値が未設定なので、aが決まらないという欠点がある。
