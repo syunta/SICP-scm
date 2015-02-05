8cfde2d5e8e0469a950099528190ee3205e5517e
syunta <kitagawa@timedia.co.jp>
2015-02-05 20:27:27 +0900

    Answer 3.47 again

--- chapter-3/3.4.2/3.47.scm
+++ chapter-3/3.4.2/3.47.scm
@@ -5,15 +5,19 @@
         (mutex (make-mutex)))
     (define (acquire)
       (mutex 'acquire)
-      (cond ((1 <= avail)
-             (set! avail (- avail 1)))
-            (else (mutex 'release)
-                  (acquire))) ;retry
-      (mutex 'release))
+      (cond ((<= 1 avail)
+             (set! avail (- avail 1))
+             (mutex 'acquire))
+            (else
+              (mutex 'release)
+              (acquire)))) ;retry
     (define (release)
       (mutex 'acquire)
-      (set! avail (+ avail 1))
-      (mutex 'release))
+      (cond ((= n avail)
+             (set! avail (+ avail 1))
+             (mutex 'release))
+            (else
+              (mutex 'relese))))
     (define (the-semaphore m)
       (cond ((eq? m 'acquire) (acquire))
             ((eq? m 'release) (release))))
@@ -27,16 +31,18 @@
     (define (acquire)
       (if (test-and-set! cell)
         (acquire) ;retry
-        (cond ((1 <= avail)
+        (cond ((<= 1 avail)
                (set! avail (- avail 1))
                (clear! cell))
-              (else (clear! cell)
-                    (acquire))))) ;retry
+              (else
+                (clear! cell)
+                (acquire))))) ;retry
     (define (release)
       (cond ((test-and-set! cell)
              (release)) ; retry
-            (else (set! avail (+ avail 1))
-                  (clear! cell))))
+            (else
+              (set! avail (+ avail 1))
+              (clear! cell))))
     (define (the-semaphore m)
       (cond ((eq? m 'acquire) (acquire))
             ((eq? m 'release) (release))))
