f3966730d77643a9c04b88bf893c3ed0c468d941
syunta <kitagawa@timedia.co.jp>
2015-02-22 22:26:32 +0900

    Define stream-filter

--- lib/stream.scm
+++ lib/stream.scm
@@ -36,6 +36,14 @@
       low
       (stream-enumerate-interval (+ low 1) high))))
 
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
 (define (display-stream s)
   (stream-for-each display-line s))
 
