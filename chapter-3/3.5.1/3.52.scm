df84d9fd2c2e9115cd5511bf5cd9437d60912b6f
syunta <kitagawa@timedia.co.jp>
2015-03-07 15:30:33 +0900

    Answer 3.52

---
+++ chapter-3/3.5.1/3.52.scm
@@ -0,0 +1,42 @@
+(load "../../lib/stream")
+
+(define sum 0)
+
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+
+(stream-ref y 7)
+
+(display-stream z)
+
+;各式が評価し終わった時のsum: 210
+
+;stream-refとdisplay-streamの評価に応じて印字される応答
+
+;(stream-ref y 7)
+;=> nothing
+;(display-stream z)
+;=>
+;10
+;15
+;45
+;55
+;105
+;120
+;190
+;210
+
+
+;memo-procの用意する最適化を使わなかった時
+
+;(display-stream z)の印字する結果が違う
+;15
+;180
+;230
+;305
