bbe10f0b62bbb8f538059a25b63b2a65b39db8bb
syunta <kitagawa@timedia.co.jp>
2015-03-07 15:58:58 +0900

    Answer 3.54

---
+++ chapter-3/3.5.2/3.54.scm
@@ -0,0 +1,15 @@
+(load "../../lib/stream")
+
+(define factorials
+  (cons-stream 1
+               (mul-streams (stream-cdr integers)
+                            factorials)))
+
+(display-stream (stream-take factorials 5))
+;=>
+;1
+;2
+;6
+;24
+;120
+;720
--- lib/stream.scm
+++ lib/stream.scm
@@ -69,6 +69,9 @@
 (define (add-streams s1 s2)
   (stream-map + s1 s2))
 
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
 (define ones (cons-stream 1 ones))
 
 (define integers (cons-stream 1 (add-streams ones integers)))
