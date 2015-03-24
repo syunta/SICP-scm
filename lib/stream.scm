353e499e6465e79e7bc76db9c1da15f023f19fb0
syunta <kitagawa@timedia.co.jp>
2015-03-24 12:43:40 +0900

    Define euler-transform

--- lib/stream.scm
+++ lib/stream.scm
@@ -107,3 +107,10 @@
                              guesses)))
   guesses)
 
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
