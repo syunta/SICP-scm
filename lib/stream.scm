557c8fde6a43ebbd223e957cca6d1cd053ecf79a
syunta <kitagawa@timedia.co.jp>
2015-03-24 12:47:08 +0900

    Define accelerated-sequence

--- lib/stream.scm
+++ lib/stream.scm
@@ -119,3 +119,7 @@
   (cons-stream s
                (make-tableau transform
                              (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
