8f1686ddf30c4ba21ae6502597b8b631cb65d93a
syunta <kitagawa@timedia.co.jp>
2015-04-01 20:00:32 +0900

    Answer 3.62 again

--- chapter-3/3.5.2/3.62.scm
+++ chapter-3/3.5.2/3.62.scm
@@ -1,19 +1,24 @@
 (load "./3.61")
 
 (define (invert-series stream)
-  (scale-stream (cons-stream (stream-car stream)
-                             (mul-series (stream-cdr stream)
-                                         (negate-stream (invert-series stream))))
-                (/ (stream-car stream))))
+  (define inverts
+    (scale-stream (cons-stream 1
+                               (negate-stream (mul-series (stream-cdr stream)
+                                                          inverts)))
+                  (/ 1 (stream-car stream))))
+  inverts)
 
 (define (div-series s1 s2)
   (if (zero? (stream-car s2))
     (error "Division by zero")
     (mul-series s1
                 (invert-series s2))))
+
 ;正接のべき級数
+(define tan-series (div-series sine-series cosine-series))
+
 (display-stream
-  (stream-take (div-series sine-series cosine-series) 7))
+  (stream-take tan-series 7))
 ;=>
 ;0
 ;1
