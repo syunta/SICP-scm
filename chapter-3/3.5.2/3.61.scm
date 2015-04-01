a048d64fdf1918113b0eb5ac784f4bc861242421
syunta <kitagawa@timedia.co.jp>
2015-04-01 19:10:25 +0900

    Improve invert-unit-series

--- chapter-3/3.5.2/3.61.scm
+++ chapter-3/3.5.2/3.61.scm
@@ -1,9 +1,11 @@
 (load "./3.60")
 
 (define (invert-unit-series s)
-  (cons-stream 1
-               (mul-series (stream-cdr s)
-                           (negate-stream (invert-unit-series s)))))
+  (define inverts
+    (cons-stream 1
+                 (mul-series (stream-cdr s)
+                             (negate-stream inverts))))
+  inverts)
 ;test
 (display-stream
   (stream-take (invert-unit-series (invert-unit-series exp-series)) 5))
