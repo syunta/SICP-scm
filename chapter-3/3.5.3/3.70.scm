83f1993ea3c8f6b75233dc4f2cde3d601fbeba4e
syunta <kitagawa@timedia.co.jp>
2015-04-26 00:08:19 +0900

    Answer 3.70 - Define weighted-pairs

--- chapter-3/3.5.3/3.70.scm
+++ chapter-3/3.5.3/3.70.scm
@@ -13,3 +13,12 @@
                   (else
                     (cons-stream s1car
                                  (merge-weighted weight (stream-cdr s1) s2))))))))
+
+(define (weighted-pairs weight s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (merge-weighted
+      weight
+      (stream-map (lambda (x) (list (stream-car s) x))
+                  (stream-cdr t))
+      (weighted-pairs weight (stream-cdr s) (stream-cdr t)))))
