f598819eabdee073e8c1f2bf9408ffaa7644a60f
syunta <kitagawa@timedia.co.jp>
2015-04-27 08:45:11 +0900

    Answer 3.76 - Define smooth

---
+++ chapter-3/3.5.3/3.76.scm
@@ -0,0 +1,14 @@
+(load "./3.74")
+
+(define sense-data (stream 1 2 1.5 1 0.5 -0.1 -2 -3 -2 -0.5 0.2 3 4))
+
+(define (smooth s last-value)
+  (cons-stream (average (stream-car s) last-value)
+               (smooth (stream-cdr s)
+                       (stream-car s))))
+
+(define (main args)
+  (display-stream
+    (stream-take (smooth sense-data 0) 11))
+  ;=> 0.5 1.5 1.75 1.25 0.75 0.2 -1.05 -2.5 -2.5 -1.25 -0.15 1.6
+  )
