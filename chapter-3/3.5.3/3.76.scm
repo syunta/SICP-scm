080e3f00cdb2920ad1aafaf21253a411f0c1b1a9
syunta <kitagawa@timedia.co.jp>
2015-04-27 09:02:44 +0900

    Answer 3.76 - Use smooth

--- chapter-3/3.5.3/3.76.scm
+++ chapter-3/3.5.3/3.76.scm
@@ -7,8 +7,22 @@
                (smooth (stream-cdr s)
                        (stream-car s))))
 
+(define (make-zero-crossings input-stream last-value)
+  (cons-stream
+    (sign-change-detector (stream-car input-stream) last-value)
+    (make-zero-crossings (stream-cdr input-stream)
+                         (stream-car input-stream))))
+
+(define zero-crossings
+  (make-zero-crossings (smooth sense-data 0)
+                       0))
+
 (define (main args)
   (display-stream
     (stream-take (smooth sense-data 0) 11))
   ;=> 0.5 1.5 1.75 1.25 0.75 0.2 -1.05 -2.5 -2.5 -1.25 -0.15 1.6
+  (newline)
+  (display-stream
+    (stream-take zero-crossings 11))
+  ;=> 0 0 0 0 0 0 1 0 0 0 0 -1
   )
