32f4beb83ab8d2e4ab6005aedb4172a62738c57d
syunta <kitagawa@timedia.co.jp>
2015-04-30 19:47:13 +0900

    Answer 3.67 again

--- chapter-3/3.5.3/3.67.scm
+++ chapter-3/3.5.3/3.67.scm
@@ -4,8 +4,8 @@
   (cons-stream
     (list (stream-car s) (stream-car t))
     (interleave
-      (stream-map (lambda (x) (list x (stream-car s)))
-                  (stream-cdr t))
+      (stream-map (lambda (x) (list x (stream-car t)))
+                  (stream-cdr s))
       (interleave
         (stream-map (lambda (x) (list (stream-car s) x))
                     (stream-cdr t))
