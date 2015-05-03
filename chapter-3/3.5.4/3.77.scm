6457f2878dd1a894b58e87d7baa9b21b57b31396
syunta <kitagawa@timedia.co.jp>
2015-05-03 12:09:55 +0900

    Answer 3.77

--- chapter-3/3.5.4/3.77.scm
+++ chapter-3/3.5.4/3.77.scm
@@ -1,13 +1,14 @@
 (load "../../lib/stream")
 
-(define (integral integrand initial-value dt)
+(define (integral delayed-integrand initial-value dt)
   (cons-stream initial-value
-               (if (stream-null? integrand)
-                 the-empty-stream
-                 (integral (stream-cdr integrand)
-                           (+ (* dt (stream-car integrand))
-                              initial-value)
-                           dt))))
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                   the-empty-stream
+                   (integral (delay (stream-cdr integrand))
+                             (+ (* dt (stream-car integrand))
+                                initial-value)
+                             dt)))))
 
 (define (solve f y0 dt)
   (define y (integral (delay dy) y0 dt))
