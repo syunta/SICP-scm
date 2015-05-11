431df24a0ea327646facb7371be78177a4eb69fb
syunta <kitagawa@timedia.co.jp>
2015-05-12 02:37:43 +0900

    Answer 3.79

---
+++ chapter-3/3.5.4/3.79.scm
@@ -0,0 +1,27 @@
+(load "../../lib/stream")
+
+;  +-------+             +--------+
+;  |map: f |---- ddy --->|integral|<-- dy0
+;  +-------+             +--------+
+;    ^   ^                   |
+;    |   |                   |
+;    |   +------- dy --------+
+;    |                       |
+;    |                       V
+;    |                   +--------+
+;    +------------ y ----|integral|<-- y0
+;                        +--------+
+
+(define integral delayed-integral)
+
+(define (solve-2nd f dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (stream-map f y dy))
+  y)
+
+(define f (lambda (y dy) (+ (* 2 dy) (* 1 y))))
+
+(define (main args)
+  (display-stream
+    (stream-take (solve-2nd f 0.01 1 0) 10)))
