f932bafc8b791e56e3afb3ff3e4c8f343f439c40
syunta <kitagawa@timedia.co.jp>
2015-05-15 02:42:47 +0900

    Answer 3.82 - Define estimate-integral

--- chapter-3/3.5.5/3.82.scm
+++ chapter-3/3.5.5/3.82.scm
@@ -21,7 +21,26 @@
     (next (+ passed 1) failed)
     (next passed (+ failed 1))))
 
+; area-of-circle = πr^2
+; => π = π * 1^2
 (define (unit-circle-test x y)
   (<= (+ (square (- x 0))
          (square (- y 0)))
       (square 1)))
+
+(define (estimate-integral P x1 x2 y1 y2)
+  (define area-of-square
+    (* (- x1 x2) (- y1 y2)))
+  (define experiments
+    (stream-map P
+                (random-in-range-stream x1 x2)
+                (random-in-range-stream y1 y2)))
+  (scale-stream (monte-carlo experiments 0 0)
+                area-of-square))
+
+(define (main args)
+  (print
+    (inexact (stream-ref (estimate-integral unit-circle-test 5 -3 6 -2)
+                         1000000)))
+  ;=> 3.127740872259128
+  )
