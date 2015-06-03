75e29423907ff14c0bdbafae9fda4ede9727ebd0
syunta <kitagawa@timedia.co.jp>
2015-06-03 19:39:21 +0900

    Fix smooth - receive one only argument

--- chapter-3/3.5.3/3.76.scm
+++ chapter-3/3.5.3/3.76.scm
@@ -2,10 +2,8 @@
 
 (define sense-data (stream 1 2 1.5 1 0.5 -0.1 -2 -3 -2 -0.5 0.2 3 4))
 
-(define (smooth s last-value)
-  (cons-stream (average (stream-car s) last-value)
-               (smooth (stream-cdr s)
-                       (stream-car s))))
+(define (smooth s)
+  (stream-map average s (cons-stream 0 s)))
 
 (define (make-zero-crossings input-stream last-value)
   (cons-stream
@@ -14,12 +12,12 @@
                          (stream-car input-stream))))
 
 (define zero-crossings
-  (make-zero-crossings (smooth sense-data 0)
+  (make-zero-crossings (smooth sense-data)
                        0))
 
 (define (main args)
   (display-stream
-    (stream-take (smooth sense-data 0) 11))
+    (stream-take (smooth sense-data) 11))
   ;=> 0.5 1.5 1.75 1.25 0.75 0.2 -1.05 -2.5 -2.5 -1.25 -0.15 1.6
   (newline)
   (display-stream
