fc82b8e59e67297d430ca9c0cb7283df2942fc0c
syunta <kitagawa@timedia.co.jp>
2015-07-20 15:37:57 +0900

    Answer 4.6

--- chapter-4/4.1.1/4.6.scm
+++ chapter-4/4.1.1/4.6.scm
@@ -1,3 +1,5 @@
+(load "../../lib/eval-apply")
+
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
@@ -9,6 +11,7 @@
          (make-procedure (lambda-parameters exp)
                          (lambda-body exp)
                          env))
+        ((let? exp) (eval (let->combination exp) env))
         ((begin? exp)
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (eval (cond->if exp) env))
@@ -17,3 +20,33 @@
                 (list-of-values (operands exp) env)))
         (else
           (error "Unknown expression type -- EVAL" exp))))
+
+(define (let-bindings exp) (cadr exp))
+
+(define (let-body exp) (cddr exp))
+
+(define (let-variables bindings)
+  (map car bindings))
+
+(define (let-expressions bindings)
+  (map cadr bindings))
+
+(define (let->combination exp)
+  (let ((bindings (let-bindings exp)))
+    (cons (make-lambda (let-variables bindings)
+                       (let-body exp))
+          (let-expressions bindings))))
+
+(define (main args)
+
+  (define let-expression
+    '(let ((var1 exp1) (var2 exp2))
+       body))
+
+  (print (let->combination let-expression))
+  ;=>
+  ;((lambda (var1 var2)
+  ;   body)
+  ; exp1
+  ; exp2)
+  )
