ae22ad3d123cfe72f232671599f8a9ad1a7d0489
syunta <kitagawa@timedia.co.jp>
2015-07-20 15:15:37 +0900

    Improve 4.5

--- chapter-4/4.1.1/4.5.scm
+++ chapter-4/4.1.1/4.5.scm
@@ -1,3 +1,5 @@
+(load "../../lib/eval-apply")
+
 (define (=>-clause? clause) (eq? (cadr clause) '=>))
 (define (cond-test clause) (car clause))
 (define (cond-recipient clause) (caddr clause))
@@ -7,7 +9,7 @@
 
 (define (expand-clauses clauses)
   (if (null? clauses)
-    'false                          ; else節なし
+    'false ; else節なし
     (let ((first (car clauses))
           (rest (cdr clauses)))
       (if (cond-else-clause? first)
@@ -16,11 +18,27 @@
           (error "ELSE clause isn't last -- COND->IF"
                  clauses))
         (if (=>-clause? first)
-          (let ((test (cond-test first)))
-            (make-if test
-                     ;2回testしてるのでtestに副作用がある場合不適切な変換だが、make-letがないので仕方ない
-                     (list (cond-recipient first) test)
-                     (expand-clauses rest)))
+          ; clause で _x を使っていた場合、変数が衝突してしまう
+          (list (make-lambda '_x
+                             (list (make-if '_x
+                                            (list (cond-recipient first) '_x)
+                                            (expand-clauses rest))))
+                (cond-test first))
           (make-if (cond-predicate first)
                    (sequence->exp (cond-actions first))
                    (expand-clauses rest)))))))
+
+(define (main args)
+
+  (define cond-expression
+    '(cond ((assoc 'b '((a 1) (b 2))) => cadr)
+           (else false)))
+
+  (print (cond->if cond-expression))
+  ;=>
+  ;((lambda _x
+  ;   (if _x
+  ;     (cadr _x)
+  ;     false))
+  ; (assoc 'b '((a 1) (b 2))))
+  )
