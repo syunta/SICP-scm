76653a7ea319b67ea5c7e2899639e66c1ef2b039
syunta <kitagawa@timedia.co.jp>
2015-09-15 21:26:17 +0900

    Answer 4.16 - b

--- chapter-4/4.1.6/4.16.scm
+++ chapter-4/4.1.6/4.16.scm
@@ -20,8 +20,51 @@
       var
       (error "Unassigned variable" var))))
 
+; b
+
+(define internal-definition
+  '(lambda '<vars>
+     (define u '<e1>)
+     (define v '<e2>)
+     '<e3>))
+
+(define internal-definition-body (cddr internal-definition))
+
+(define let-assignment
+  '(lambda '<vars>
+     (let ((u *unassigned*)
+           (v *unassigned*))
+       (set! u '<e1>)
+       (set! v '<e2>)
+       '<e3>)))
+
+(define let-assignment-body (cddr let-assignment))
+
+(define (lambda-defines exp)
+  (filter (lambda (x) (eq? 'define (car x))) exp))
+
+(define (lambda-expressions exp)
+  (filter (lambda (x) (not (eq? 'define (car x)))) exp))
+
+(define (scan-out-defines exps)
+  (let ((vars (map definition-variable (lambda-defines exps)))
+        (vals (map definition-value (lambda-defines exps))))
+    (list ;bodyは式のリスト
+      (make-let (map (lambda (var) (list var '*unassigned*))
+                     vars)
+                (append (map (lambda (var val) (make-assignment var val))
+                             vars vals)
+                        (lambda-expressions exps))))))
+
 (define (main args)
-  (driver-loop)
+  ;(driver-loop)
   ; (define x '*unassigned*) => ok
   ; x => Unassigned variable *unassigned*
+
+  (print (scan-out-defines internal-definition-body))
+  ;=>
+  ;((let ((u *unassigned*) (v *unassigned*)) (set! u '<e1>) (set! v '<e2>) '<e3>))
+  (print (equal? (scan-out-defines internal-definition-body)
+                 let-assignment-body))
+  ;=> #t
   )
