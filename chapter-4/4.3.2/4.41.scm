102410a6732191ac8d9a169c8bb13f1f9764901f
syunta <kitagawa@timedia.co.jp>
2015-11-06 19:24:54 +0900

    Answer 4.41 - Add improved answer

--- chapter-4/4.3.2/4.41.scm
+++ chapter-4/4.3.2/4.41.scm
@@ -70,8 +70,62 @@
                                 (smith '(1 2 3 4 5)))
                             (quintuples baker cooper fletcher miller smith))))))))))))
 
+; 前段の制限で既に除外されたのではない可能性だけの生成を基にした解き方
+; より効率的になったはず
+(define (adjoin x y) (append x (list y)))
+
+(define (adjoin-possibilities new-ps ps)
+  (flatmap
+    (lambda (p)
+      (map (lambda (new-p) (adjoin p new-p))
+           new-ps))
+    ps))
+
+(define (multiple-dwelling-improved)
+  (let ((baker-possibility '(1 2 3 4 5))
+        (cooper-possibility '(1 2 3 4 5))
+        (fletcher-possibility '(1 2 3 4 5))
+        (miller-possibility '(1 2 3 4 5))
+        (smith-possibility '(1 2 3 4 5)))
+    (map
+      (lambda (x)
+        (list (list 'baker (baker x))
+              (list 'cooper (cooper x))
+              (list 'fletcher (fletcher x))
+              (list 'miller (miller x))
+              (list 'smith (smith x))))
+      (filter
+        distinct?
+        (filter
+          (lambda (x) (not (= (abs (- (smith x) (fletcher x))) 1)))
+          (adjoin-possibilities
+            smith-possibility
+            (filter
+              (lambda (x) (not (= (abs (- (fletcher x) (cooper x))) 1)))
+              (filter
+                (lambda (x) (> (miller x) (cooper x)))
+                (adjoin-possibilities
+                  miller-possibility
+                  (filter
+                    (lambda (x) (not (= (fletcher x) 1)))
+                    (filter
+                      (lambda (x) (not (= (fletcher x) 5)))
+                      (adjoin-possibilities
+                        fletcher-possibility
+                        (filter
+                          (lambda (x) (not (= (cooper x) 1)))
+                          (adjoin-possibilities
+                            cooper-possibility
+                            (filter
+                              (lambda (x) (not (= (baker x) 5)))
+                              (map
+                                (lambda (x) (list x))
+                                baker-possibility))))))))))))))))
+
 (define (main args)
   (print (multiple-dwelling))
+  (print (multiple-dwelling-improved))
   ;=>
+  ; (((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1)))
   ; (((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1)))
   )
