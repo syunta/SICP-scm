e1ae4f4267c7ad4c2d3466ad971ca090cdb9a092
syunta <kitagawa@timedia.co.jp>
2016-01-07 13:57:42 +0900

    Answer 4.51 - Support permanent-set!

--- chapter-4/4.3.3/4.51.scm
+++ chapter-4/4.3.3/4.51.scm
@@ -1,18 +1,59 @@
 (add-load-path "../../lib" :relative)
 (load "amb")
 
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((permanent-assignment? exp) (analyze-permanent-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((amb? exp) (analyze-amb exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((let? exp) (analyze (let->combination exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((application? exp) (analyze-application exp))
+        (else
+          (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (permanent-assignment? exp)
+  (tagged-list? exp 'permanent-set!))
+
+(define (analyze-permanent-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (set-variable-value! var val env)
+               (succeed 'ok
+                        (lambda ()
+                          (fail2))))
+             fail))))
+
 (for-each simple-ambeval
           '(
             (define count 0)
+            (define count2 0)
             ))
 
 (define (main args)
   (print-ambeval
     '(let ((x (an-element-of '(a b c)))
            (y (an-element-of '(a b c))))
-       (set! count (+ count 1))
+       (permanent-set! count (+ count 1))
        (require (not (eq? x y)))
        (list x y count))
+    10)
+  (print-ambeval
+    '(let ((x (an-element-of '(a b c)))
+           (y (an-element-of '(a b c))))
+       (set! count2 (+ count2 1))
+       (require (not (eq? x y)))
+       (list x y count2))
     10)
   ;=>
   ; (a b 1)
