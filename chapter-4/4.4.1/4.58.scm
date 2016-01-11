cb09c465041fe4b325eabc711ff7fc630e7dedb7
syunta <kitagawa@timedia.co.jp>
2016-01-11 11:00:43 +0900

    Answer 4.58 again

--- chapter-4/4.4.1/4.58.scm
+++ chapter-4/4.4.1/4.58.scm
@@ -3,13 +3,19 @@
 
 (qeval-add
   '(assert! (rule (big-shot ?division ?person)
-                  (and (job ?person (?division . ?type))
-                       (not (supervisor ?person ?who)))))) 
+                  (or (and (job ?person (?division . ?type-1))
+                           (supervisor ?person ?who)
+                           (job ?who (?division-2 . ?type-2))
+                           (not (same ?division-2 ?division)))
+                      (and (job ?person (?division . ?type-1))
+                           (not (supervisor ?person ?who)))))))
 
 (define test '(big-shot ?division ?who))
 
 (define (main args)
   (print-qeval test)
   ;=>
+  ;(big-shot accounting (Scrooge Eben))
   ;(big-shot administration (Warbucks Oliver))
+  ;(big-shot computer (Bitdiddle Ben))
   )
