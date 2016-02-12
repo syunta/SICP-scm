17cd16fb6f897ab17af9e46cbe45c9f163ba0915
syunta <kitagawa@timedia.co.jp>
2016-02-12 09:17:15 +0900

    Answer 4.73 - Add counter example against query system

--- chapter-4/4.4.4/4.73.scm
+++ chapter-4/4.4.4/4.73.scm
@@ -10,6 +10,11 @@
 
 (define ones (cons-stream 1 ones))
 
+(define assersions-one
+  (cons-stream '(one 1) assersions-one))
+
+(set! THE-ASSERTIONS assersions-one)
+
 (define (main args)
   ; 入力が無限ストリームである場合、再帰的に呼び出されるinterleaveの2番目の引数
   ; (flatten-stream (stream-cdr stream))
@@ -24,4 +29,13 @@
   ;=>
   ; 無限ループに陥る。陽にdelayを使う場合は問題ない。
   ; 質問システムにおいては、無限の表明や無限の規則を持つ場合、無限ループに陥る。
+  (print-qeval '(?x 1))
+  ;=>
+  ; 無限ループに陥り、出力が返らない。
+  ; 陽にdelayを使う場合、(one 1) が無限に出力される。
+  ; この違いは、例えば質問システムが任意の数の結果を印字し
+  ; `(try-again)`
+  ; で残りの結果を印字する、という仕様だった場合、
+  ; delayを陽に使うことでシステムが無限に印字を続けることを避けられるが、
+  ; delayを陽に使わないと無限にループ落ち込んでしまい望ましくない.
   )
