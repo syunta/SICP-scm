d0d1468d081e3a8384d1fefe66436a0dd19a17a9
syunta <kitagawa@timedia.co.jp>
2016-03-31 20:55:46 +0900

    Fix 4.69

--- chapter-4/4.4.3/4.69.scm
+++ chapter-4/4.4.3/4.69.scm
@@ -2,16 +2,26 @@
 
 (for-each (lambda (q) (qeval-add `(assert! ,q)))
           '(
+            (rule (end-of-grandson (grandson)))
+            (rule (end-of-grandson (?x . ?y))
+                  (end-of-grandson ?y))
+
             (rule ((great grandson) ?x ?y)
                   (and (grandson ?x ?gs)
                        (son ?gs ?y)))
+
             (rule ((great . ?rel) ?x ?y)
                   (and (son ?x ?s)
-                       ((?rel) ?s ?y)))
+                       (?rel ?s ?y)
+                       (end-of-grandson ?rel)))
             ))
+
+; (grandson ?x ?y)
+; (grandson . ())
 
 (define test1 '((great grandson) ?g ?ggs))
 (define test2 '(?relationship Adam Irad))
+(define test3 '((great great great grandson) ?g ?ggs))
 
 (define (main args)
   (print-qeval test1)
@@ -25,4 +35,10 @@
   (print-qeval test2)
   ;=>
   ;((great grandson) Adam Irad)
+  (print-qeval test3)
+  ;=>
+  ;((great great great grandson) Enoch Jubal)
+  ;((great great great grandson) Cain Lamech)
+  ;((great great great grandson) Enoch Jabal)
+  ;((great great great grandson) Adam Methushael)
   )
