d429567f6d3dccefd61ebfc608a2618a4ee5eb42
syunta <kitagawa@timedia.co.jp>
2016-04-05 20:41:14 +0900

    Answer 1.3 - Add another answer

--- chapter-1/1.1.6/1.3.scm
+++ chapter-1/1.1.6/1.3.scm
@@ -3,5 +3,22 @@
     (+ (square (car x))
        (square (cadr x)))))
 
-(print (f 3 9 1))
-;=> 9 * 9 + 3 * 3 = 90
+(define (f2 x y z)
+  (let ((f (lambda (a b)
+             (+ (square a)
+                (square b)))))
+  (cond ((< x y z) (f y z))
+        ((< y x z) (f x z))
+        (else (f x y)))))
+
+(define (main args)
+  (print (f 1 2 3))
+  (print (f2 1 2 3))
+  ;=> 2 * 2 + 3 * 3 = 13
+  (print (f 4 2 5))
+  (print (f2 4 2 5))
+  ;=> 4 * 4 + 5 * 5 = 41
+  (print (f 3 9 1))
+  (print (f2 3 9 1))
+  ;=> 9 * 9 + 3 * 3 = 90
+  )
