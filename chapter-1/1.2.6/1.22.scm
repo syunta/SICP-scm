3c62d7151c2e78240319035c99c14b00973a1f3e
syunta <kitagawa@timedia.co.jp>
2016-05-25 00:04:00 +0900

    Answer 1.22 - Analyze order

--- chapter-1/1.2.6/1.22.scm
+++ chapter-1/1.2.6/1.22.scm
@@ -27,13 +27,47 @@
          (search-for-primes (+ low 2) high))
         (else (search-for-primes (+ 1 low) high))))
 
+(define (take-primes low count)
+  (define (iter primes n c)
+    (cond ((= 0 c) (reverse primes))
+          ((odd? n)
+           (if (prime? n)
+             (iter (cons n primes) (+ n 2) (- c 1))
+             (iter primes (+ n 2) c)))
+          (else (iter primes (+ n 1) c))))
+  (iter '() low count))
+
+(define (take-elapsed-times low count)
+  (define (iter times n c)
+    (cond ((= 0 c) (reverse times))
+          ((odd? n)
+           (let ((start-time (runtime)))
+             (if (prime? n)
+               (let ((elapsed-time (- (runtime) start-time)))
+                 (iter (cons elapsed-time times) (+ n 2) (- c 1)))
+               (iter times (+ n 2) c))))
+          (else (iter times (+ n 1) c))))
+  (iter '() low count))
+
+(define (average seq)
+  (inexact (/ (apply + seq) (length seq))))
+
 (define (main args)
-  (search-for-primes 1000 1020)
-  ; 1009 1013 1019
-  (search-for-primes 10000 10040)
-  ; 10007 10009 10037
-  (search-for-primes 100000 100050)
-  ; 100003 100019 100043
-  (search-for-primes 1000000 1000050)
-  ; 1000003 1000033 100037
+  (print (take-primes 1000 3))
+  ;=> (1009 1013 1019)
+  (print (take-primes 10000 3))
+  ;=> (10007 10009 10037)
+  (print (take-primes 100000 3))
+  ;=> (100003 100019 100043)
+  (print (take-primes 1000000 3))
+  ;=> (1000003 1000033 1000037)
+  (print (average (take-elapsed-times 1000 3)))
+  ;=> 8
+  (print (average (take-elapsed-times 10000 3)))
+  ;=> 25
+  (print (average (take-elapsed-times 100000 3)))
+  ;=> 70
+  (print (average (take-elapsed-times 1000000 3)))
+  ;=> 200
+  ; 概ね sqrt(10) 倍ずつになっている。
   )
