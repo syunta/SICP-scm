b31c0c050549d48fb6e5740ca278863f39ad610f
syunta <kitagawa@timedia.co.jp>
2016-06-08 22:15:08 +0900

    Answer 1.18 - Add Russian peasant method

--- chapter-1/1.2.4/1.18.scm
+++ chapter-1/1.2.4/1.18.scm
@@ -8,9 +8,35 @@
         ((even? b) (mul-iter (double a) (halve b) p))
         (else (mul-iter a (- b 1) (+ p a)))))
 
+; ロシア農民の方法
+;
+;   45 * 113
+;
+;   45   113   +45
+;   90    56
+;  180    28
+;  360    14
+;  720     7   +720
+; 1440     3   +1440
+; 2880     1   +2880
+;
+; = 5085
+
+(define (halve-floor x)
+  (truncate (/ x 2)))
+
+(define (russian-peasant-mul a b)
+  (mul-iter a b 0))
+
+(define (r-mul-iter a b p)
+  (cond ((= b 0) p)
+        ((even? b) (r-mul-iter (double a) (halve-floor b) p))
+        (else (r-mul-iter (double a) (halve-floor b) (+ p a)))))
+
 (define (main args)
   (print (mul 3 1))
   (print (mul 3 2))
   (print (mul 3 3))
   (print (mul 3 4))
+  (print (russian-peasant-mul 45 113))
   )
