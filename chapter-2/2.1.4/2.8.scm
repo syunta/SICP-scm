9ef4fa34736b671127c7f4d46f3632c5e17b889c
syunta <kitagawa@timedia.co.jp>
2016-06-13 18:19:43 +0900

    Answer 2.8

---
+++ chapter-2/2.1.4/2.8.scm
@@ -0,0 +1,13 @@
+(load "./2.7")
+
+; 区間x,yの差の最小値はxの下限とyの上限の差であり、最大値はxの上限とyの下限の差である。
+; X - Y = [a - d, b - c]
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (main args)
+  (print (sub-interval (make-interval 2 5)
+                       (make-interval 0 2)))
+  )
