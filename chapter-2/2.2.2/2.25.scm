8bc45d8b82d86b3405aacd3da47ab509c29bf1c2
syunta <kitagawa@timedia.co.jp>
2016-07-03 14:06:26 +0900

    Answer 2.25

---
+++ chapter-2/2.2.2/2.25.scm
@@ -0,0 +1,9 @@
+(define (main args)
+  (print
+    (car (cdr (car (cdr (cdr '(1 3 (5 7) 9)))))))
+  (print
+    (car (car '((7)))))
+  (print
+    (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr
+    '(1 (2 (3 (4 (5 (6 7)))))))))))))))))))
+  )
