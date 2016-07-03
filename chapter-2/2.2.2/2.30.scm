00073b78762a3ab7a5f534ff8dfc6097b0036e79
syunta <kitagawa@timedia.co.jp>
2016-07-03 17:57:54 +0900

    Answer 2.30

---
+++ chapter-2/2.2.2/2.30.scm
@@ -0,0 +1,21 @@
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree-2 tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree-2 sub-tree)
+           (square sub-tree)))
+       tree))
+
+(define (main args)
+  (print (square-tree (list 1
+                            (list 2 (list 3 4) 5)
+                            (list 6 7))))
+  (print (square-tree-2 (list 1
+                              (list 2 (list 3 4) 5)
+                              (list 6 7))))
+  )
