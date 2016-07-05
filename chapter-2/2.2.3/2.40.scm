c75c645a95e6bc273e2e24d311973df70e0fd0f6
syunta <kitagawa@timedia.co.jp>
2016-07-05 19:41:28 +0900

    Answer 2.40

---
+++ chapter-2/2.2.3/2.40.scm
@@ -0,0 +1,34 @@
+(load "./2.39")
+(use math.prime)
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define prime? miller-rabin-prime?)
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
+
+(define (main args)
+  (print (unique-pairs 5))
+  (print (prime-sum-pairs 6))
+  )
