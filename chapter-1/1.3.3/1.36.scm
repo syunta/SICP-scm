705a019cfef78e268be2373beac44dcec4ea8b6b
syunta <kitagawa@timedia.co.jp>
2016-07-20 19:44:56 +0900

    Refactor 1.36 and Add explanation

--- chapter-1/1.3.3/1.36.scm
+++ chapter-1/1.3.3/1.36.scm
@@ -14,24 +14,25 @@
         (try next))))
   (try first-guess))
 
-(define (f1 x)
+(define (f1)
   (fixed-point (lambda (x) (/ (log 1000) (log x)))
                2.0))
 
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (f2 x)
+(define (f2)
   (fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
                2.0))
 
 (define (main args)
-  (f1 1000)
+  (f1)
   (print count)
   ;=> 34
   (set! count 0)
-  (f2 1000)
+  (f2)
   (print count)
   ; 9
   ; 平均緩和を使った方が早く収束する
+  ; 平均緩和法を使うと振動を抑えられる
   )
