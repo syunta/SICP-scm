133dcb55f8db74dcadd7e3c7e0e6e52d3e724081
syunta <kitagawa@timedia.co.jp>
2016-10-04 21:36:03 +0900

    Answer 5.39 - Define lexical-address-set!

--- chapter-5/5.5.6/5.39.scm
+++ chapter-5/5.5.6/5.39.scm
@@ -2,12 +2,20 @@
 (load "../../lib/load-eceval-compiler")
 (load "../../lib/compiler")
 
-(define (lexical-address-lookup fnum dnum env)
-  (let ((vals (cdr (list-ref env fnum))))
-    (let ((val (list-ref vals dnum)))
+(define (make-address fnum dnum) (list fnum dnum))
+(define (frame-number address) (car address))
+(define (displacement-number address) (cadr address))
+
+(define (lexical-address-lookup address env)
+  (let ((vals (cdr (list-ref env (frame-number address)))))
+    (let ((val (list-ref vals (displacement-number address))))
       (if (eq? val '*unassigned*)
         (error "LEXICAL-ADDRESS-LOOKUP - *unassigned* variable")
         val))))
+
+(define (lexical-address-set! address val env)
+  (let ((vals (cdr (list-ref env (frame-number address)))))
+    (list-set! vals (displacement-number address) val)))
 
 (define (main args)
   (define test
@@ -15,5 +23,8 @@
           (make-frame '(x y) '(5 6))))
 
   (print (lookup-variable-value 'x test))
-  (print (lexical-address-lookup 1 0 test))
+  (print (lexical-address-lookup (make-address 1 0) test))
+
+  (lexical-address-set! (make-address 1 0) 10 test)
+  (print (lexical-address-lookup (make-address 1 0) test))
   )
