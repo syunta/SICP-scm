16f5a463665c5805f139edb712f1b36ce8e216fc
syunta <kitagawa@timedia.co.jp>
2016-10-11 20:43:08 +0900

    Answer 5.49

---
+++ chapter-5/5.5.7/5.49.scm
@@ -0,0 +1,42 @@
+(add-load-path "../../lib" :relative)
+(load "../../lib/load-eceval-compiler")
+(load "../../lib/compiler")
+
+(define (get-eccompile) eccompile)
+
+(define (start-eccompile)
+  (set! the-global-environment (setup-environment))
+  (start eccompile))
+
+(define eccompile
+  (make-machine
+    '(exp env val proc argl continue unev arg1 arg2 compapp machine)
+    (append eceval-operations
+            (list (list 'get-eccompile get-eccompile)
+                  (list 'compile compile)
+                  (list 'statements statements)
+                  (list 'assemble assemble)))
+    '(
+read-compile-execute-print-loop
+      (assign machine (op get-eccompile))
+      (perform (op initialize-stack))
+      (perform
+        (op prompt-for-input) (const ";;; EC-Compile input:"))
+      (assign exp (op read))
+      (assign env (op get-global-environment))
+      (assign val (op compile) (reg exp) (const val) (const return))
+      (assign val (op statements) (reg val))
+      (assign val (op assemble) (reg val) (reg machine))
+      (assign continue (label print-result))
+      (goto (reg val))
+print-result
+      (perform (op print-stack-statistics))
+      (perform
+        (op announce-output) (const ";;; EC-Compile value:"))
+      (perform (op user-print) (reg val))
+      (goto (label read-compile-execute-print-loop))
+      )))
+
+(define (main args)
+  (start-eccompile)
+  )
