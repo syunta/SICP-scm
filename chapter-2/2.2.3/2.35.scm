e143d6b663b3c9ea5c39c7f811be9d881a14b3fb
syunta <kitagawa@timedia.co.jp>
2016-10-12 20:32:08 +0900

    Add count-leaves-2

--- chapter-2/2.2.3/2.35.scm
+++ chapter-2/2.2.3/2.35.scm
@@ -9,6 +9,19 @@
                        (count-leaves x)))
                    t)))
 
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves-2 t)
+  (accumulate +
+              0
+              (map (lambda (t) 1)
+                   (enumerate-tree t))))
+
 (define (main args)
   (print (count-leaves '(1 2 (3 4) (5 (6 (7) 8 (9))))))
+  (print (count-leaves-2 '(1 2 (3 4) (5 (6 (7) 8 (9))))))
   )
