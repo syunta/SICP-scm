017af94b0ec963bd8bb42ea0c20428723a26bef0
syunta <kitagawa@timedia.co.jp>
2016-10-24 01:42:21 +0900

    Fix 2.46

--- chapter-2/2.2.4/2.46.scm
+++ chapter-2/2.2.4/2.46.scm
@@ -6,14 +6,14 @@
 
 (define (add-vect v1 v2)
   (make-vect (+ (xcor-vect v1)
-                (xcor-vect v1))
-             (+ (ycor-vect v2)
+                (xcor-vect v2))
+             (+ (ycor-vect v1)
                 (ycor-vect v2))))
 
 (define (sub-vect v1 v2)
   (make-vect (- (xcor-vect v1)
-                (xcor-vect v1))
-             (- (ycor-vect v2)
+                (xcor-vect v2))
+             (- (ycor-vect v1)
                 (ycor-vect v2))))
 
 (define (scale-vect s v)
