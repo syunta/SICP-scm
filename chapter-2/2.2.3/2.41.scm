b5c8eb354a4ee1ee1cf4d076a258a0ca1b52121b
syunta <kitagawa@timedia.co.jp>
2016-10-24 11:43:58 +0900

    Add unique-ns

--- chapter-2/2.2.3/2.41.scm
+++ chapter-2/2.2.3/2.41.scm
@@ -6,6 +6,17 @@
                   (enumerate-interval 1 (- (cadr ij) 1))))
            (unique-pairs n)))
 
+(define (unique-ns d n)
+  (if (< d 3)
+    (flatmap (lambda (i)
+                 (map (lambda (j) (list i j))
+                      (enumerate-interval 1 (- i 1))))
+               (enumerate-interval 2 n))
+    (flatmap (lambda (rest)
+               (map (lambda (new) (append rest (list new)))
+                    (enumerate-interval 1 (- (list-ref rest (- d 2)) 1))))
+             (unique-ns (- d 1) n))))
+
 (define (triple-sum-equal s n)
   (filter (lambda (ijk)
             (= s (accumulate + 0 ijk)))
@@ -13,5 +24,6 @@
 
 (define (main args)
   (print (unique-triples 5))
+  (print (unique-ns 3 5))
   (print (triple-sum-equal 10 5))
   )
