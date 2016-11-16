22cecf4734f78ef15074f5e927c0f9225986d3d9
syunta <kitagawa@timedia.co.jp>
2016-11-16 21:12:42 +0900

    Improve 2.60

--- chapter-2/2.3.3/2.60.scm
+++ chapter-2/2.3.3/2.60.scm
@@ -7,7 +7,7 @@
 (define (adjoin-set x set)
   (cons x set))
 
-; O(n^2) -> O(1)
+; O(n^2) -> O(n) appendはO(n)
 (define (union-set set1 set2)
   (append set1 set2))
 
@@ -27,5 +27,6 @@
   (print (intersection-set '(2 3 2 1 3 2 2) '(4 4 1 4 3)))
   ;=> (3 1 3 1 3)
 
-  ; union-setは効率が良いので、頻繁にunion-setを使う必要がある場合に重複あり表現を使いたくなる。
+  ; adjoin-setやunion-setは効率が良いので、頻繁にadjoin-set, union-setを使う必要がある場合に重複あり表現を使いたくなる。
+  ; 例えば、DBなどレコードが集合で表現されているとして、頻繁に追加操作(adjoin-set)を行う必要がある場合は重複を許す表現を選択すると良い。
   )
