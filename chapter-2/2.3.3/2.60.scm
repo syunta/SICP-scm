a4b2fd4068d897044e21459f0e76b787161f3121
syunta <kitagawa@timedia.co.jp>
2016-11-16 13:35:49 +0900

    Answer 2.60

--- chapter-2/2.3.3/2.60.scm
+++ chapter-2/2.3.3/2.60.scm
@@ -1,36 +1,31 @@
-(define (element-of-set? x set)
-  (define (test x set n)
-    (cond ((null? set) #f)
-          ((equal? x (car set)) #t)
-          (else (test x (cdr set) (counter)))))
-  (test x set (counter)))
+(load "./2.59")
 
+; O(n) -> O(n)
+; element-of-set? は変更不要
+
+; O(n) -> O(1)
 (define (adjoin-set x set)
   (cons x set))
 
+; O(n^2) -> O(1)
+(define (union-set set1 set2)
+  (append set1 set2))
+
+; O(n^2) -> O(n^2 + m^2)
 (define (intersection-set set1 set2)
-  (define (test set1 set2 n)
+  (define (go set1 set2)
     (cond ((or (null? set1) (null? set2)) '())
           ((element-of-set? (car set1) set2)
-           (cons (car set1)
-                 (test (cdr set1) set2 (counter))))
-          (else (test (cdr set1) set2 (counter)))))
-  (test set1 set2 (counter)))
+           (cons (car set1) (go (cdr set1) set2)))
+          (else (go (cdr set1) set2))))
+  (append (go set1 set2)
+          (go set2 set1)))
 
-(define (union-set set1 set2)
-  (define (test set1 set2 n)
-    (cond ((null? set2) set1)
-          ((null? set1) set2)
-          ((element-of-set? (car set1) set2)
-           (test (cdr set1) set2 (counter)))
-          (else (test (cdr set1) (cons (car set1) set2) (counter)))))
-  (test set1 set2 (counter)))
+(define (main args)
+  (print (union-set '(2 3 2 1 3 2 2) '(4 4 1 4 3)))
+  ;=> (2 3 2 1 3 2 2 4 4 1 4 3)
+  (print (intersection-set '(2 3 2 1 3 2 2) '(4 4 1 4 3)))
+  ;=> (3 1 3 1 3)
 
-[define (count-generator)
-  (let ((count 0))
-    (lambda () (set! count (+ count 1))))]
-
-(define counter (count-generator))
-
-(print (union-set '(1 4 8) '(1 2 5 6 7)))
-(print (- (counter) 1))
+  ; union-setは効率が良いので、頻繁にunion-setを使う必要がある場合に重複あり表現を使いたくなる。
+  )
