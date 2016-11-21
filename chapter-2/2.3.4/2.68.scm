ed1ba36e138a768886ece9d473702524ebafb691
syunta <kitagawa@timedia.co.jp>
2016-11-21 18:15:09 +0900

    Answer 2.68

---
+++ chapter-2/2.3.4/2.68.scm
@@ -0,0 +1,26 @@
+(load "./2.67")
+
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))))
+
+(define (encode-symbol s tree)
+  (define (iter tree bits)
+    (cond ((leaf? tree)
+           (reverse bits))
+          ((memq s (symbols (left-branch tree)))
+           (iter (left-branch tree) (cons 0 bits)))
+          ((memq s (symbols (right-branch tree)))
+           (iter (right-branch tree) (cons 1 bits)))
+          (else
+            (error "ENCODE-SYMBOL --Symbol Not Found"))))
+  (iter tree '()))
+
+(define (main args)
+  (print (encode '(A D A B B C A) sample-tree))
+  ;=> (0 1 1 0 0 1 0 1 0 1 1 1 0)
+  (print (equal? sample-message
+                 (encode '(A D A B B C A) sample-tree)))
+  )
