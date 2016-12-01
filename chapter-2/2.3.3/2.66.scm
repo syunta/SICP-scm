942a72929f3982ab5676abbc9637a6bdc0211750
syunta <kitagawa@timedia.co.jp>
2016-12-01 11:39:56 +0900

    Imporove 2.66 - Add test case

--- chapter-2/2.3.3/2.66.scm
+++ chapter-2/2.3.3/2.66.scm
@@ -9,11 +9,25 @@
       (8  (name e) (age 26))
       (11 (name f) (age 23)))))
 
+(define personnel-db-1
+  '((1  (name a) (age 26))
+    (2  (name b) (age 24))
+    (3  (name c) (age 28))
+    (5  (name d) (age 26))
+    (8  (name e) (age 26))
+    (11 (name f) (age 23))))
+
 (define (key record)
   (car record))
 
 (define (contents record)
   (cdr record))
+
+(define (lookup-1 given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (car set-of-records)))
+         (car set-of-records))
+        (else (lookup-1 given-key (cdr set-of-records)))))
 
 (define (lookup given-key db)
   (if (null? db)
@@ -27,6 +41,8 @@
              (lookup given-key (right-branch db)))))))
 
 (define (main args)
+  (print (lookup-1 5 personnel-db-1))
+  ;=> (5 (name d) (age 26))
   (print (lookup 5 personnel-db))
   ;=> ((name d) (age 26))
   )
