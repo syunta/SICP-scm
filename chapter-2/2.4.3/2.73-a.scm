251ec90fd58ef3cf23cec7b7d41c95bbca493a63
syunta <kitagawa@timedia.co.jp>
2016-12-06 19:41:42 +0900

    Fix 2.73

--- chapter-2/2.4.3/2.73-a.scm
+++ chapter-2/2.4.3/2.73-a.scm
@@ -1,113 +1,35 @@
-(add-load-path "../.." :relative)
-(load "lib/library.scm")
+(load "./2.73")
 
 (define (deriv ex var)
   ((get 'deriv (operator ex)) (operands ex) var))
 
-(define (operator ex) (car ex))
-(define (operands ex) (cdr ex))
-
-(define (variable? e) (symbol? e))
-
-(define (same-variable? x y)
-  (and (variable? x) (variable? y) (eq? x y)))
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
 (define (install-number-package)
-  (define (deriv-num operands var) 0)
-
-  (put 'deriv 'num deriv-num)
-  'number-done)
+  (define (num operands var) 0)
+  (put 'deriv 'num num)
+  'done)
 
 (define (install-variable-package)
-  (define (deriv-var operands var)
-    (let ((ex (car operands)))
-      (if (same-variable? ex var) 1 0)))
-
-  (put 'deriv 'var deriv-var)
-  'variable-done)
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
-
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-
-(define (install-sum-package)
-  (define (deriv-sum operands var)
-    (let ((ex (apply make-sum operands)))
-    (make-sum
-      (deriv (addend ex) var)
-      (deriv (augend ex) var))))
-
-  (put 'deriv '+ deriv-sum)
-  'sum-done)
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-
-(define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
-
-(define (install-product-package)
-  (define (deriv-product operands var)
-    (let ((ex (apply make-product operands)))
-      (make-sum
-        (make-product (multiplier ex)
-                      (deriv (multiplicand ex) var))
-        (make-product (deriv (multiplier ex) var)
-                      (multiplicand ex)))))
-
-  (put 'deriv '* deriv-product)
-  'product-done)
-
-(define (base e) (cadr e))
-(define (exponent e) (caddr e))
-
-(define (make-exponentiation x y)
-  (cond ((=number? y 0) 1)
-        ((=number? y 1) x)
-        (else (list '** x y))))
-
-(define (install-exponentiation-package)
-  (define (deriv-exponentiation operands var)
-    (let ((ex (apply make-exponentiation operands)))
-      (make-product
-        (exponent ex)
-        (make-product (make-exponentiation (base ex)
-                                           (- (exponent ex) 1))
-                      (deriv (base ex) var)))))
-
-  (put 'deriv '** deriv-exponentiation)
-  'exponentiation-done)
+  (define (var operands var)
+    (let ((exp (car operands)))
+      (if (same-variable? exp var) 1 0)))
+  (put 'deriv 'var var)
+  'done)
 
 (define (print-deriv seq)
   (let ((ex (car seq)) (var (cadr seq)))
     (print (deriv ex var))))
 
 (define (main args)
-  (print (install-number-package))
-  (print (install-variable-package))
-  (print (install-sum-package))
-  (print (install-product-package))
-  (print (install-exponentiation-package))
+  (install-number-package)
+  (install-variable-package)
+  (install-sum-package)
+  (install-product-package)
+  (install-exponentiation-package)
   (map print-deriv
        (list '((* (var x) (num 4)) x)
              '((* (var x) (var y)) x)
              '((+ (var x) (num 3)) x)
-             '((* (* (var x) (var y)) (+ (var x) (num 3))) x))))
-
+             '((* (* (var x) (var y)) (+ (var x) (num 3))) x)))
+  )
 ; こんな感じで変数や数値もリストにしてタグを付ければテーブルに吸収できる。
 ; 問題で吸収できないと言っている理由は、変数や数値の形式を変えてはならないから
--- chapter-2/2.4.3/2.73.scm
+++ chapter-2/2.4.3/2.73.scm
@@ -1,89 +1,60 @@
-(add-load-path "../.." :relative)
-(load "lib/library.scm")
+(add-load-path "../../lib" :relative)
+(add-load-path "../2.3.2" :relative)
+(load "table")
+(load "2.56")
 
-(define (deriv ex var)
-  (cond
-    ((number? ex) 0)
-    ((variable? ex)
-     (if (same-variable? ex var) 1 0))
-    (else ((get 'deriv (operator ex)) (operands ex) var))))
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp) var))))
 
-(define (operator ex) (car ex))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
 
-(define (operands ex) (cdr ex))
-
-(define (variable? e)
-  (symbol? e))
-
-(define (same-variable? x y)
-  (and (variable? x) (variable? y) (eq? x y)))
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
-
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-
+; b
 (define (install-sum-package)
   (define (deriv-sum operands var)
-    (let ((ex (apply make-sum operands)))
+    (let ((exp (apply make-sum operands)))
     (make-sum
-      (deriv (addend ex) var)
-      (deriv (augend ex) var))))
-
+      (deriv (addend exp) var)
+      (deriv (augend exp) var))))
   (put 'deriv '+ deriv-sum)
   'done)
 
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-
-(define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
-
 (define (install-product-package)
   (define (deriv-product operands var)
-    (let ((ex (apply make-product operands)))
+    (let ((exp (apply make-product operands)))
       (make-sum
-        (make-product (multiplier ex)
-                      (deriv (multiplicand ex) var))
-        (make-product (deriv (multiplier ex) var)
-                      (multiplicand ex)))))
-
+        (make-product (multiplier exp)
+                      (deriv (multiplicand exp) var))
+        (make-product (deriv (multiplier exp) var)
+                      (multiplicand exp)))))
   (put 'deriv '* deriv-product)
   'done)
 
-(define (base e) (cadr e))
-(define (exponent e) (caddr e))
-
-(define (make-exponentiation x y)
-  (cond ((=number? y 0) 1)
-        ((=number? y 1) x)
-        (else (list '** x y))))
-
+; c
 (define (install-exponentiation-package)
   (define (deriv-exponentiation operands var)
-    (let ((ex (apply make-exponentiation operands)))
+    (let ((exp (apply make-exponentiation operands)))
       (make-product
-        (exponent ex)
-        (make-product (make-exponentiation (base ex)
-                                           (- (exponent ex) 1))
-                      (deriv (base ex) var)))))
-
+        (exponent exp)
+        (make-product (make-exponentiation (base exp)
+                                           (- (exponent exp) 1))
+                      (deriv (base exp) var)))))
   (put 'deriv '** deriv-exponentiation)
   'done)
 
 (define (main args)
-  (print (install-sum-package))
-  (print (install-product-package))
-  (print (install-exponentiation-package))
-  (print (deriv '(** x 4) 'x)))
+  ; a
+  ; 数値や変数はタグ付けできないので吸収できない
+  (install-sum-package)
+  (install-product-package)
+  (install-exponentiation-package)
+  (print (deriv '(+ x 4) 'x))
+  ;=> 1
+  (print (deriv '(* x 4) 'x))
+  ;=> 4
+  (print (deriv '(** x 4) 'x))
+  ;=> (* 4 (** x 3))
+  )
