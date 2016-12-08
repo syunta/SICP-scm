06ce09182d1fe4c5ff97bf88f2271b52bf31af52
syunta <kitagawa@timedia.co.jp>
2016-12-08 14:54:16 +0900

    Simplify 2.74

--- chapter-2/2.4.3/2.74.scm
+++ chapter-2/2.4.3/2.74.scm
@@ -1,22 +1,22 @@
-(add-load-path "../.." :relative)
-(load "lib/library.scm")
+(add-load-path "../../lib" :relative)
+(load "table")
 
 ; a
 ;
 ; ファイルに対して追加されるべき型情報は、社員情報の集合の構造
 ; list, ordered-list, binary-tree, etc ...
-
+;
 ; レコードに対して追加されるべき型情報は、レコードの集合の構造
 ; (name salary address), (address name salary), etc ... 
-
+;
 ; (key value)の構造も事業所毎に異なるので、
 ; これも追加されるべき型情報である
 ; (key value), (value . key), (value), etc ...
-
+;
 ; これ以上の型情報の追加は不毛なので、ここまでにしておく
-
+;
 ; 追加される型情報が複数になったので、タグをリストで扱う
-
+;
 ; 通常、 name はアルファベットで表現されるが
 ; この順序付けを実装していると問題の本筋から外れてしまうので
 ; 今回は name を数値で表現することにする
@@ -25,34 +25,34 @@
 ;
 ; この演算が働くためには、引数として受け取るレコードが、
 ; レコードの集合の型情報を持つよう構造化されている必要がある
- 
+;
 ; よって、a で実装した get-record は record をそのまま返していたが、
 ; これに、型情報を付加した上で record を返す修正が必要となる
-
+;
 ; 実装の統一感を出すため、get-name , get-value の引数に対しても、型情報を含める形に変更を加えた
 
 ; c
 ;
 ; name に重複があった場合は、リストにまとめて返すこととした
-; これまで get-recordでは record が見つからなかった場合、エラーを返していたが、 nil を返すよう修正しておく
 
 ; d
 ;
 ; これまでに実装してきた (install-***-package) のように、
 ; 合併した事業所のデータ構造にあわせて package を追加してあげればよい
 
-(define (find-employee-record files name)
-    (if (null? files)
-      nil
-      (let ((record (get-record (car files) name)))
-        (if (null? record)
-          (find-employee-record (cdr files) name)
-          (cons (contents record) (find-employee-record (cdr files) name))))))
+(define (get-record file name)
+  (let ((tags (type-tag file)))
+    ((get 'get-record (file-type tags)) name file)))
 
-(define (get-record personnel-file name)
-  (let ((tags (type-tag personnel-file)))
-    ((get 'get-record (file-type tags)) name
-                                        personnel-file)))
+(define (find-employee-record files name)
+  (if (null? files)
+    '()
+    (let ((record (get-record (car files) name)))
+      (if (null? record)
+        (find-employee-record (cdr files) name)
+        (cons (contents record)
+              (find-employee-record (cdr files) name))))))
+
 (define (get-name record)
   (let ((tags (type-tag record)))
     ((get 'get-name (record-type tags)) record)))
@@ -83,47 +83,42 @@
 (define (key-value-type tags) (caddr tags))
 
 (define (install-unordered-list-package)
-  (define (lookup given-key personnel-file)
-    (let ((set-of-records (contents personnel-file))
-          (tags (type-tag personnel-file)))
-      (if (null? set-of-records)
-        nil
-        (let ((record (attach-tag tags (car set-of-records))))
-          (if (= given-key (get-name record))
-            record
-            (lookup given-key
-                    (attach-tag tags (cdr set-of-records))))))))
-  (put 'get-record 'unordered-list lookup)
+  (define (get-record given-key file)
+    (define tags (type-tag file))
+    (define (lookup set-of-records)
+      (cond ((null? set-of-records) '())
+            ((= given-key (get-name (attach-tag tags (car set-of-records))))
+             (car set-of-records))
+            (else (lookup (cdr set-of-records)))))
+    (attach-tag tags (lookup (contents file))))
+  (put 'get-record 'unordered-list get-record)
   'done)
 
 (define (install-ordered-list-package)
-  (define (lookup given-key personnel-file)
-    (let ((set-of-records (contents personnel-file))
-          (tags (type-tag personnel-file)))
+  (define (get-record given-key file)
+    (define tags (type-tag file))
+    (define (lookup set-of-records)
       (if (null? set-of-records)
-        nil
-        (let ((record (attach-tag tags (car set-of-records))))
-          (let ((name (get-name record)))
-            (cond ((< given-key name) nil)
-                  ((= given-key name) record)
-                  (else (lookup given-key
-                                (attach-tag tags (cdr set-of-records))))))))))
-  (put 'get-record 'ordered-list lookup)
+        '()
+        (let ((name (get-name (attach-tag tags (car set-of-records)))))
+          (cond ((< given-key name) '())
+                ((= given-key name) (car set-of-records))
+                (else (lookup (cdr set-of-records)))))))
+    (attach-tag tags (lookup (contents file))))
+  (put 'get-record 'ordered-list get-record)
   'done)
 
 (define (install-jp-record-package)
   (define (get-name tag-record)
     (let ((tags (type-tag tag-record))
           (record (contents tag-record)))
-      (let ((name-value (attach-tag tags (car record))))
-        (get-value name-value))))
-
+      (let ((tagged-key-value (attach-tag tags (car record))))
+        (get-value tagged-key-value))))
   (define (get-salary tag-record)
     (let ((tags (type-tag tag-record))
           (record (contents tag-record)))
-      (let ((name-value (attach-tag tags (cadr record))))
-        (get-value name-value))))
-
+      (let ((tagged-key-value (attach-tag tags (cadr record))))
+        (get-value tagged-key-value))))
   (put 'get-name 'jp get-name)
   (put 'get-salary 'jp get-salary)
   'done)
@@ -132,38 +127,38 @@
   (define (get-name tag-record)
     (let ((tags (type-tag tag-record))
           (record (contents tag-record)))
-      (let ((name-value (attach-tag tags (cadr record))))
-        (get-value name-value))))
-
+      (let ((tagged-key-value (attach-tag tags (cadr record))))
+        (get-value tagged-key-value))))
   (define (get-salary tag-record)
     (let ((tags (type-tag tag-record))
           (record (contents tag-record)))
-      (let ((name-value (attach-tag tags (caar record))))
-        (get-value name-value))))
-
+      (let ((tagged-key-value (attach-tag tags (caar record))))
+        (get-value tagged-key-value))))
   (put 'get-name 'us get-name)
   (put 'get-salary 'us get-salary)
   'done)
 
-(define (install-plain-key-value-package)
-  (define (get-value tag-key-value) (contents tag-key-value))
-  (put 'get-value 'plain get-value)
+(define (install-dot-list-key-value-package)
+  (define (get-value tagged-key-value)
+    (cdr (contents tagged-key-value)))
+  (put 'get-value 'dot-list get-value)
   'done)
 
-(define (install-standard-key-value-package)
-  (define (get-value tag-key-value) (cadr (contents tag-key-value)))
-  (put 'get-value 'standard get-value)
+(define (install-list-key-value-package)
+  (define (get-value tagged-key-value)
+    (cadr (contents tagged-key-value)))
+  (put 'get-value 'list get-value)
   'done)
 
-(define jp-file (attach-tag '(ordered-list jp standard)
+(define jp-file (attach-tag '(ordered-list jp list)
                             '(((name 2) (salary 1000) (address Tokyo  ))
                               ((name 6) (salary 2000) (address Nagano ))
                               ((name 8) (salary 7000) (address Tottori)))))
 
-(define us-file (attach-tag '(unordered-list us plain)
-                            '(((10 L.A    ) 5)
-                              ((15 NewYork) 2)
-                              ((40 Boston ) 7))))
+(define us-file (attach-tag '(unordered-list us dot-list)
+                            '((((salary . 10) (address . L.A)) (name . 5))
+                              (((sarary . 15) (address . NewYork)) (name . 2))
+                              (((salary . 40) (address . Boston)) (name . 7)))))
 
 (define files (list jp-file us-file))
 
@@ -172,8 +167,9 @@
   (install-ordered-list-package)
   (install-jp-record-package)
   (install-us-record-package)
-  (install-plain-key-value-package)
-  (install-standard-key-value-package)
-  (print (get-salary (get-record jp-file '6)))
-  (print (get-salary (get-record us-file '7)))
-  (print (find-employee-record files '2)))
+  (install-dot-list-key-value-package)
+  (install-list-key-value-package)
+  (print (get-salary (get-record jp-file 6)))
+  (print (get-salary (get-record us-file 7)))
+  (print (find-employee-record files 2))
+  )
